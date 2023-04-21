589cc2ad9c4cba689ef83beec6305a168e26bf70
MinhThien203 <trinhminhthien17112003@gmail.com>
2023-04-21 22:39:35 +0700

    init project

---
+++ app/Http/Controllers/CustomAuthController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Session;
+
+class CustomAuthController extends Controller
+{
+
+    public function index()
+    {
+        return view('auth.login');
+    }
+
+    public function customLogin(Request $request)
+    {
+        $request->validate([
+            'email' => 'required',
+            'password' => 'required',
+        ]);
+
+        $credentials = $request->only('email', 'password');
+        if (Auth::attempt($credentials)) {
+            return redirect()->intended('dashboard')
+                ->withSuccess('Signed in');
+        }
+
+        return redirect("login")->withSuccess('Login details are not valid');
+    }
+
+    public function registration()
+    {
+        return view('auth.registration');
+    }
+
+    public function customRegistration(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6',
+            'phone' => ['nullable', 'string', 'min:10'],
+        ]);
+
+        if ($request->hasFile('image')) {
+            $file = $request->file('image');
+            $ext = $file->getClientOriginalExtension();
+            $filename = time() . '.' . $ext;
+            $file->move('assets/uploads/', $filename);
+        }
+
+        $data = $request->all();
+        $data['image'] = $filename;
+
+        $check = $this->create($data);
+        $check->save();
+
+
+        return redirect("dashboard")->withSuccess('You have signed-in');
+    }
+
+    public function create(array $data)
+    {
+        return User::create([
+            'name' => $data['name'],
+            'phone' => $data['phone'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password']),
+            'image' => $data['image'],
+        ]);
+    }
+
+    public function dashboard()
+    {
+        if (Auth::check()) {
+            $user = User::all();
+            return view('listuser', compact('user'));
+        }
+
+        return redirect("login")->withSuccess('You are not allowed to access');
+    }
+
+    public function signOut()
+    {
+        Session::flush();
+        Auth::logout();
+
+        return Redirect('login');
+    }
+}
